b579211c7b922bb31671bbdacc6053365bf34211
tye-exe <tye@mailbox.org>
2024-06-22 08:51:59 +0100

    Got rust working

--- tye/rust.nix
+++ tye/rust.nix
@@ -9,21 +9,38 @@
       # Installs the rust toolchain
       rustc
       # Install well, useful stuff.
-      rustup
+      #      rustup
+      # Rust LSP
+      rust-analyzer
+      cargo
+      clippy
       # Rust debugger
       lldb
       # Live debugger
       bacon
       # Creates cache
 
+      # Mold as rust linker
+      clang
+      mold
     ];
 
-    file."settings.toml" = {
+    file."rustup_config" = {
       target = ".rustup/settings.toml";
       text = std.serde.toTOML {
         default_toolchain = "stable-x86_64-unknown-linux-gnu";
         profile = "default";
         version = "12";
+      };
+    };
+
+    file."cargo_config" = {
+      target = ".cargo/config.toml";
+      text = std.serde.toTOML {
+        target.stable-x86_64-unknown-linux-gnu = {
+          linker = "${pkgs.clang}/bin/clang";
+          rustflags = ''["-C", "link-arg=--ld-path=${pkgs.mold}/bin/mold"]'';
+        };
       };
     };
   };
