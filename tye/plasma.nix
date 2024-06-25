0e018bf4485ae854e3e42998d318bebea511cd12
tye-exe <tye@mailbox.org>
2024-06-25 10:51:49 +0100

    Configured pannel

--- tye/plasma.nix
+++ tye/plasma.nix
@@ -10,6 +10,7 @@
     enable = true;
 
     workspace = {
+      # I like my eyes, thank you very much
       lookAndFeel = "org.kde.breezedark.desktop";
 
       cursor = {
@@ -23,5 +24,43 @@
       key = "Ctrl+Alt+T";
       command = "rio";
     };
+
+    panels = [{
+      location = "top";
+      height = 48;
+      widgets = [
+        # Launcher menu
+        "org.kde.plasma.kickoff"
+        # Has the open apps on it
+        {
+          name = "org.kde.plasma.icontasks";
+          config = {
+            General.launchers = [
+              "applications:org.kde.dolphin.desktop"
+              "applications:firefox.desktop"
+              "applications:rio.desktop"
+            ];
+          };
+        }
+        "org.kde.plasma.marginsseparator"
+        {
+          systemTray.items = {
+            # We explicitly show bluetooth and battery
+            shown = [
+              "org.kde.plasma.clipboard"
+              # "org.kde.plasma.volume"
+              "org.kde.plasma.volume"
+              "org.kde.plasma.bluetooth"
+            ];
+            # And explicitly hide networkmanagement and volume
+            hidden = [
+              "org.kde.plasma.networkmanagement"
+              # "org.kde.plasma.volume"
+            ];
+          };
+        }
+        { digitalClock = { }; }
+      ];
+    }];
   };
 }
