e1f21d235e1c967bd5d6514f47fa8159d90f82d5
tye-exe <tye@mailbox.org>
2024-08-12 12:00:19 +0100

    Added auto updates once a day

--- system/utils.nix
+++ system/utils.nix
@@ -1,12 +1,35 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     # These are like useful utils, yk?
-    # zip
-    # unzip
     _7zz # Seven zip
 
     # Disk management
     gparted
     ntfs3g # Ntfs support
   ];
+
+  system.autoUpgrade = let
+    nixDir = "/home/tye/nixos";
+    # Reads in the which flake direvation to use.
+    variant = builtins.readFile /home/tye/nixos/.identity;
+    # Gets the root config for the direvation.
+    # As the root file is the same name with the prefix removed.
+    conf-file = builtins.replaceStrings [ "tye-" ] [ "" ] variant;
+  in {
+    enable = true;
+    operation = "switch";
+    flake = "${nixDir}/#${variant}";
+    flags = [
+      "-I"
+      "nixos-config=${nixDir}/${conf-file}"
+      "--update-input"
+      "nixpkgs"
+      "--impure"
+      "-L" # print build logs
+    ];
+    # Timing doesn't matter due to persistence
+    dates = "00:00";
+    persistent = true;
+  };
+
 }
