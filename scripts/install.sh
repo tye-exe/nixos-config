436c9f2f8c9588c4af3890e3bc7fad349790e872
tye-exe <tye@mailbox.org>
2024-08-25 13:16:56 +0100

    eval now explicitly calls bash

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-bash
 
 # Download repo & move into it.
 git clone https://github.com/tye-exe/nixos-config.git
@@ -9,16 +8,16 @@ cd nixos-config
 chmod +x core.lua 
 
 # Prints my logo; This can be removed if desired.
-eval ./core.lua logo
+./core.lua logo
 
 # Generate hardware conf for this machine.
 echo "Generating hardware configuration for this machine."
 nixos-generate-config --show-hardware-config >> ./hardware-confs/undefined.nix
 
 echo "Switching system configuration."
-sudo eval ./core.lua undefined-sys-switch
+sudo bash eval ./core.lua undefined-sys-switch
 
 echo "Switching home-manager configuration."
-eval ./core.lua undefined-hm-switch
+eval bash ./core.lua undefined-hm-switch
 
 echo "Finished configuration system & dot files; Reboot to allow for configuration to take full effect."
