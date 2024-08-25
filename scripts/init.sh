416ac73c10c8b7ab58a547f5ca037ccfccd158ba
Tye <131195812+tye-exe@users.noreply.github.com>
2024-08-25 13:09:39 +0100

    Fixed Typo

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,5 +2,5 @@
 
 # Prerequisites to initialise configuration.
 nix --extra-experimental-features "nix-command flakes" \
-shell nixpkgs#git nixpkgs#lua nixpkgs#curl nipkgs#bash \
+shell nixpkgs#git nixpkgs#lua nixpkgs#curl nixkgs#bash \
 --command sh -c "curl -s https://raw.githubusercontent.com/tye-exe/nixos-config/main/scripts/install.sh | bash"
