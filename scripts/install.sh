09ae42d7dd5970ebf72ea92e3c754007fed8911c
tye-exe <tye@mailbox.org>
2024-08-25 17:26:02 +0100

    Fixed issues with nix-flakes & repo already being cloned

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,9 +6,17 @@ set -eo pipefail
 # Only continues execution if sudo permission is granted.
 sudo echo "Sudo permission granted."
 
-# Download repo & move into it.
-git clone https://github.com/tye-exe/nixos-config.git
-cd nixos-config
+# Check if repo has already been cloned.
+if [ -d "nixos-config" ] then
+  echo "Repo found; updating"
+  cd nixos-config
+  git pull
+else
+  echo "Cloning repo."
+  # Download repo & move into it.
+  git clone https://github.com/tye-exe/nixos-config.git
+  cd nixos-config
+fi
 
 # Sets the lua script to be executable.
 chmod +x core.lua 
@@ -19,6 +27,8 @@ chmod +x core.lua
 # Generate hardware conf for this machine.
 echo "Generating hardware configuration for this machine."
 nixos-generate-config --show-hardware-config >> ./hardware-confs/undefined.nix
+# A file needs to be added to git for nix flakes to access it.
+git add ./hardware-confs/undefined.nix
 
 echo "Switching system configuration."
 eval ./core.lua undefined-sys-switch | sudo bash
@@ -26,4 +36,4 @@ eval ./core.lua undefined-sys-switch | sudo bash
 echo "Switching home-manager configuration."
 eval ./core.lua undefined-hm-switch | bash
 
-echo "Finished configuration system & dot files; Reboot to allow for configuration to take full effect."
+echo "Finished configuring system & dot files; Reboot to allow for the configuration to take full effect."
