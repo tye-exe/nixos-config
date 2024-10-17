fc64aaaf11beb5a569e84eb89f195ccedb1a8beb
tye-exe <tye@mailbox.org>
2024-10-17 17:47:41 +0100

    Added `heroic` game launcher to desktop

--- home/desktop.nix
+++ home/desktop.nix
@@ -12,17 +12,21 @@
     ./preset/de.nix
   ];
 
-  home.packages = [
-    # Minceraft
-    pkgs.modrinth-app
-    # Java for Mc
-    # pkgs.jdk8
-    pkgs.jdk17_headless
-    # pkgs.openjdk21_headless
+  home.packages =
+    with pkgs;
+    [
+      # Minceraft
+      modrinth-app
+      # Java for Mc
+      # pkgs.jdk8
+      jdk17_headless
+      # pkgs.openjdk21_headless
 
-    pkgs.obs-studio
-    pkgs-unstable.ytmdesktop
-  ];
+      obs-studio
+      # Games
+      heroic
+    ]
+    ++ (with pkgs-unstable; [ ytmdesktop ]);
 
   services.syncthing.enable = true;
 
