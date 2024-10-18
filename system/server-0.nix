13991497810da4cbd183fbc9e15e99653d9543c0
tye-exe <tye@mailbox.org>
2024-10-18 09:18:20 +0100

    Updated server config from rebase with nixfmt-rfc-style

--- system/server-0.nix
+++ system/server-0.nix
@@ -1,15 +1,21 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, ... }:
+{
   # Shared configs are in this file.
   imports = [ ./core.nix ];
 
   networking = {
     hostName = "tye-server-0";
-    nameservers = [ "1.1.1.1" "8.8.8.8" ]; # Cloudflair, Google
+    nameservers = [
+      "1.1.1.1"
+      "8.8.8.8"
+    ]; # Cloudflair, Google
     defaultGateway = "192.168.0.1";
-    interfaces.eno1.ipv4.addresses = [{
-      address = "192.168.0.33";
-      prefixLength = 24;
-    }];
+    interfaces.eno1.ipv4.addresses = [
+      {
+        address = "192.168.0.33";
+        prefixLength = 24;
+      }
+    ];
     firewall.enable = false;
   };
 
@@ -21,7 +27,9 @@
   users.users."tye".extraGroups = [ "docker" ];
 
   # No gui
-  programs = { firefox.enable = false; };
+  programs = {
+    firefox.enable = false;
+  };
 
   environment.systemPackages = with pkgs; [ iptables ];
   systemd.services."reroute_ports" = {
@@ -30,7 +38,6 @@
       ${pkgs.iptables}/bin/iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 3443;
     '';
     wantedBy = [ "multi-user.target" ];
-    description =
-      "Rerouts traffic from port 80 to port 3080; Rerouts traffic from port 443 to port 3443.";
+    description = "Rerouts traffic from port 80 to port 3080; Rerouts traffic from port 443 to port 3443.";
   };
 }
