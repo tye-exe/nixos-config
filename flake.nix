aad558d9d78545ee255ca66cde83d097993d2f22
tye-exe <tye@mailbox.org>
2024-12-02 00:20:35 +0000

    Change nixpkgs to 24.11

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "My flake(?)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # ~/ files config
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
