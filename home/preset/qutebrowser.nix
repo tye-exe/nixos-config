0e21ab1478a4ac21bd9ad3048ab957ea29e63a54
tye-exe <tye@mailbox.org>
2025-05-22 20:38:29 +0100

    Quteborowser adblocking
    Updated the adblocking list to incude those from uBlockOrigin

--- home/preset/qutebrowser.nix
+++ home/preset/qutebrowser.nix
@@ -35,6 +35,30 @@
       };
 
       content.blocking.method = "both";
+      content.blocking.adblock.lists = [
+        # Default
+        "https://easylist.to/easylist/easylist.txt"
+        "https://easylist.to/easylist/easyprivacy.txt"
+
+        # Ublock
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt"
+        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"
+      ];
 
       downloads.location.remember = false;
 
