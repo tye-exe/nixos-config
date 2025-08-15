91145a577b978842f9297aafb964013ecfc9bc9a
tye-exe <tye@mailbox.org>
2025-08-15 20:48:43 +0100

    Add laptop ssh key

--- system/core.nix
+++ system/core.nix
@@ -96,8 +96,8 @@
       "wheel"
     ];
     openssh.authorizedKeys.keys = [
-      # Desktop
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGu/8cJc3bf0RQhigvzxQPYPrGBR4WiFP6x3nB8JtsMj tye"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGu/8cJc3bf0RQhigvzxQPYPrGBR4WiFP6x3nB8JtsMj tye" # Desktop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBevXkSxiJ6RqYr8tih3Ha8G6nKF/FIA2kqIAxr+RkG tye" # Laptop
     ];
   };
 
