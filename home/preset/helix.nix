cefd2e6552ce112aa08684eb05a256e87cfee5eb
tye-exe <tye@mailbox.org>
2025-08-17 13:01:29 +0100

    Configure PHP actor

--- home/preset/helix.nix
+++ home/preset/helix.nix
@@ -118,6 +118,12 @@ in
         command = "superhtml";
         args = [ "lsp" ];
       };
+
+      # PHP
+      phpactor = {
+        command = "phpactor";
+        args = [ "language-server" ];
+      };
     };
 
     # language configs
