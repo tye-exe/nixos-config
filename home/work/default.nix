7b4acb0984bcb33a58f729ed98d8c1cf6176edc8
tye-exe <tye@mailbox.org>
2026-03-24 15:35:39 +0000

    Add ideavim configuration

--- home/work/default.nix
+++ home/work/default.nix
@@ -2,6 +2,7 @@
   pkgs,
   inputs,
   system,
+  lib,
   ...
 }:
 {
@@ -22,6 +23,19 @@
         viu # Terminal image viewer
         ripgrep # Faster alternative to grep
         caligula # Every time i need this i have to spend 5 minuets searching for it
+        remmina
+
+        # C# development
+        jetbrains.rider
+        (
+          with dotnetCorePackages;
+          combinePackages [
+            sdk_8_0
+            sdk_10_0
+          ]
+        )
+
+        libreoffice
       ]
       ++ (with inputs.nix-alien.packages.${system}; [
         nix-alien
@@ -49,5 +63,51 @@
       name = "tye-travcomuk";
       email = "tye@travcomuk.com";
     };
+  };
+
+  tye-services.enabled = {
+    syncthingtray = lib.mkForce false;
+    keyboard = lib.mkForce false;
+  };
+
+  home.file."ideavim" = {
+    enable = true;
+    target = ".ideavimrc";
+    text = ''
+      let mapleader=" "
+
+      set which-key
+      set easymotion
+
+      " Increase the timeout to make the possible keys pop-up useable
+      set timeoutlen=5000
+
+      " Recommended settings
+      map <leader>f <Plug>(easymotion-s)
+      map <leader>e <Plug>(easymotion-f)
+
+      map <leader>d <Action>(Debug)
+      map <leader>r <Action>(RenameElement)
+      map <leader>c <Action>(Stop)
+      map <leader>z <Action>(ToggleDistractionFreeMode)
+
+      map <leader>s <Action>(SelectInProjectView)
+      "map <leader>a <Action>(Annotate)
+      map <leader>h <Action>(Vcs.ShowTabbedFileHistory)
+      map <S-Space> <Action>(GotoNextError)
+
+      map <leader>b <Action>(ToggleLineBreakpoint)
+      map <leader>o <Action>(FileStructurePopup)
+
+      "Mine 
+
+      map gw <Plug>(easymotion-jumptoanywhere)
+      map <leader>k <Action>(ShowHoverInfo)
+      map <leader>a <Action>(ShowIntentionActions)
+
+      "MY EARS
+      set visualbell
+      set noerrorbells
+    '';
   };
 }
