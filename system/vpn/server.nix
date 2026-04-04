6560ebf8d0326591cdf5a34891f07bcbd2d6784e
tye-exe <tye@mailbox.org>
2026-04-04 15:34:20 +0100

    Wg conf

--- system/vpn/server.nix
+++ system/vpn/server.nix
@@ -32,14 +32,12 @@ in
     networks."50-${interface}" = {
       matchConfig.Name = interface;
 
-      # address = [
-      #   # /32 and /128 specifies a single address
-      #   # for use on this wg peer machine
-      #   # "fd31:bf08:57cb::1/128"
-      #   # "192.168.2.1/32"
-      #   "::/0"
-      #   "0.0.0.0/0"
-      # ];
+      address = [
+        # /32 and /128 specifies a single address
+        # for use on this wg peer machine
+        "fd31:bf08:57cb::1/128"
+        "192.168.2.1/32"
+      ];
 
       networkConfig = {
         IPv4Forwarding = true;
