5aac810b45b381bbf2b977902cd45be98c4b82ee
tye-exe <tye@mailbox.org>
2026-04-04 16:07:17 +0100

    Wg conf

--- system/vpn/server.nix
+++ system/vpn/server.nix
@@ -73,9 +73,10 @@ in
             PublicKey = key;
             AllowedIPs = [
               # Index + 1 because server is at 1.
-              # "fd31:bf08:57cb::${toString (index + 1)}/128"
-              # "192.168.2.${toString (index + 1)}/32"
-              "192.168.2.0/0"
+              "fd31:bf08:57cb::${toString (index + 1)}/128"
+              "192.168.2.${toString (index + 1)}/32"
+              # DO NOT USE THIS, BREAKS NETWORKING.
+              # "192.168.2.0/0"
             ];
 
             # RouteTable can also be set in wireguardPeers
