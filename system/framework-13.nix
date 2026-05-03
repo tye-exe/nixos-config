f6e33cc9cc9d70ada5dfccba54d440efe5cc30f2
tye-exe <tye@mailbox.org>
2026-05-03 16:43:22 +0100

    Add podman for framework 13

--- system/framework-13.nix
+++ system/framework-13.nix
@@ -13,6 +13,12 @@
     inputs.sops-nix.nixosModules.sops
   ];
 
+  networking.firewall = {
+    enable = true;
+    # Required for windows connecting form inside container.
+    allowedTCPPorts = [ 14240 ];
+  };
+
   networking.hostName = "framework"; # Define your hostname.
   hardware.bluetooth.enable = true;
 
@@ -43,29 +49,41 @@
     extraGroups = [
       "networkmanager"
       "wheel"
+      "podman"
     ];
     hashedPasswordFile = config.sops.secrets.initialHashedPassword.path;
   };
 
-  virtualisation.docker = {
-    enable = false;
-
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
-      # Optionally customize rootless Docker daemon settings
-      daemon.settings = {
-        dns = [
-          "1.1.1.1"
-          "8.8.8.8"
-        ];
+  virtualisation = {
+    containers.enable = true;
+    docker = {
+      enable = false;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+        # Optionally customize rootless Docker daemon settings
+        daemon.settings = {
+          dns = [
+            "1.1.1.1"
+            "8.8.8.8"
+          ];
+        };
       };
     };
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+    };
   };
+
+  environment.defaultPackages = with pkgs; [ podman-compose ];
 
   system.stateVersion = "25.11";
 
   # Allow decryption of swap
   boot.initrd.luks.devices."luks-ff21dae6-bab5-49d2-b6e3-77b6533f42fe".device =
     "/dev/disk/by-uuid/ff21dae6-bab5-49d2-b6e3-77b6533f42fe";
+
+  services.fprintd.enable = true;
 }
